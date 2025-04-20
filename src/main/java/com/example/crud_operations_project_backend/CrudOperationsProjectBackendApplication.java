7ee1c67fa2df076b75fc754c028a9aaf9a565c45
KUN4L2001 <kunalgaikwad0257@gmail.com>
2025-04-20 16:31:57 +0530

    Initial commit

---
+++ src/main/java/com/example/crud_operations_project_backend/CrudOperationsProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_operations_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationsProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationsProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crud_operations_project_backend/CrudOperationsProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_operations_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationsProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
